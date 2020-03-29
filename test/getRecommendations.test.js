ada9a4f2161ec219a12dfd2287a33a864e1863af
David Colucci <david_colucci@condenast.com>
2020-03-29 15:14:04 -0400

    more test cases

--- test/getRecommendations.test.js
+++ test/getRecommendations.test.js
@@ -1,11 +1,53 @@
 const getRecommendations = require('../src/getRecommendations.js');
 
 describe('getRecommendations', () => {
-  test('it always returns the header row', () => {
-    const matrix = [
+  let matrix;
+
+  beforeEach(() => {
+    matrix = [
+      ['header1', 'header2'],
+      ['foo', 'bar'],
+      ['foo', 'baz'],
+      ['quux', 'dix'],
+      ['trieste', 'dease']
+    ];
+  });
+
+  test('it returns the header row in a matrix containing only header row', () => {
+    matrix = [
       ['header1', 'header2']
     ];
     const result = getRecommendations({}, matrix);
     expect(result).toEqual([['header1', 'header2']]);
+  });
+
+  test('it returns the matrix if the query is empty', () => {
+    const result = getRecommendations({}, matrix);
+    expect(result).toEqual([
+      ['header1', 'header2'],
+      ['foo', 'bar'],
+      ['foo', 'baz'],
+      ['quux', 'dix'],
+      ['trieste', 'dease']
+    ]);
+  });
+
+  test('it returns all rows matching a query with 1 parameter', () => {
+    const query = { header1: 'foo' };
+    const result = getRecommendations(query, matrix);
+    expect(result).toEqual([
+      ['header1', 'header2'],
+      ['foo', 'bar'],
+      ['foo', 'baz']
+    ]);
+  });
+
+  test('it returns all rows matching intersection of 2 parameters', () => {
+    const query = { header1: 'foo', header2: 'bar' };
+    const result = getRecommendations(query, matrix);
+    expect(result).toEqual([
+      ['header1', 'header2'],
+      ['foo', 'bar']
+    ]);
   });
 });
