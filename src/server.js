9e0b0bd8ec05e27e3c167fd8032c882b6417ecad
David Colucci <david_colucci@condenast.com>
2020-04-08 20:52:29 -0400

    Adds setup to read secrets from env vars and write to local file for authentication with google

--- src/server.js
+++ src/server.js
@@ -2,18 +2,25 @@ const express = require('express');
 
 const getRecommendations = require('./getRecommendations.js');
 
+const PORT = process.env.PORT || 8000;
 const app = express();
 let _data;
 
 app.get('/recommendations', (req, res) => {
+  console.log('*request received:', req.path);
   const recommendations = getRecommendations(req.query, _data);
   res.json(recommendations);
 });
 
+app.get('/ping', (req, res) => {
+  console.log('*request received:', req.path);
+  res.json({ ok: true });
+})
+
 module.exports = async (loadData) => {
   _data = await loadData();
-  console.log(`loaded ${_data.length} entries!`);
-  app.listen(8000, () => {
-    console.log('server started on port 8000!');
+  console.log(`loaded ${_data.length} entries, yo!`);
+  app.listen(PORT, () => {
+    console.log(`server started on port ${PORT}!`);
   });
 };
